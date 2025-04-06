5a7d61d999616403244240ec62292d2543849aef
ZenLone <ilia.redka22@gmail.com>
2025-04-06 20:24:42 +0300

    daw

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from . import views
+from .views import get_recipes
 
 urlpatterns = [
-    path('', views.get_recipes, name='test'),
+    path('', get_recipes, name='test'),
 ]

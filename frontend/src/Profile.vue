e9d0857a66517e01329a06d6ae5431411c9e50c6
Dima <mishchenkodima24@gmail.com>
2025-04-10 11:34:04 +0300

    login and register

---
+++ frontend/src/Profile.vue
@@ -0,0 +1,26 @@
+<script>
+import TrueAuth from './components/trueAuth.vue';
+import trueAuth from './components/trueAuth.vue';
+import falseAuth from './components/falseAuth.vue';
+export default{
+    name:'Profile',
+    components:{
+    trueAuth,
+    falseAuth
+    },
+    setup(){
+        //data
+
+        //methods
+
+        //Хуки
+    }
+}
+</script>
+<template>
+<!-- <trueAuth></trueAuth> -->
+ <falseAuth></falseAuth>
+</template>
+<style scoped>
+
+</style>

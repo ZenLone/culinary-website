36c3a22836bc08d67d94e3652864ccd9c602dd1d
Dima <mishchenkodima24@gmail.com>
2025-04-10 20:29:26 +0300

    login and register2

--- frontend/src/Profile.vue
+++ frontend/src/Profile.vue
@@ -1,7 +1,9 @@
 <script>
-import TrueAuth from './components/trueAuth.vue';
+import Cookies from 'js-cookie';
 import trueAuth from './components/trueAuth.vue';
 import falseAuth from './components/falseAuth.vue';
+import axios from 'axios';
+import {ref} from 'vue';
 export default{
     name:'Profile',
     components:{
@@ -10,16 +12,29 @@ export default{
     },
     setup(){
         //data
-
+        const token = Cookies.get('token'); // Получаем токен из cookie
+        const isAuthenticated = ref(!!token); // Проверяем наличие токена
         //methods
+        const validateToken = async () => {
+        try {
+        const response = await axios.post('http://localhost:8000/api/validate-token/');
+        isAuthenticated.value = response.data.valid;
+         } catch (error) {
+        isAuthenticated.value = false;
+        console.error('Token validation failed:', error.message);
+        console.log('Login or register!!!');
+        }
+    };
+
 
         //Хуки
+        validateToken(); // Вызываем при загрузке компонента
     }
 }
 </script>
 <template>
-<!-- <trueAuth></trueAuth> -->
- <falseAuth></falseAuth>
+<trueAuth v-if="isAuthenticated"></trueAuth>
+ <falseAuth v-else></falseAuth>
 </template>
 <style scoped>
 
